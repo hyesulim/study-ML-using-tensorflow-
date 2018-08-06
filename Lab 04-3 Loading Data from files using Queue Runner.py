b723965b328b019a8adb742af8947acc8da1ef10
limhyesu98 <redbaloon17@gmail.com>
2018-08-06 11:23:51 +0900

    Lab 04-3 filequeue 사용하는 강의 마무리.

--- Lab 04-3 Loading Data from files using Queue Runner.py
+++ Lab 04-3 Loading Data from files using Queue Runner.py
@@ -5,7 +5,7 @@ filename_queue = tf.train.string_input_producer(
 
 # reader을 정의함.
 # binary를 읽는 reader가 될 수도 있는 등 다양함. 현재는 text를 읽도록 정의함.
-# 파일 큐에서 key와 value를 읽어오도록하였음.
+# 파일 큐에서 key와 value를 읽어오도록 하였음.
 reader = tf.TextLineReader()
 key, value= reader.read(filename_queue)
 
@@ -17,8 +17,47 @@ record_defaults = [[0.], [0.], [0.], [0.]]
 xy = tf.decode_csv(value, record_defaults=record_defaults)
 
 # xy로 읽어온 것을 batch를 이용하여 pump처럼 가져와 읽을 것임.
+# 한번에 10개씩 읽을 것임.
 train_x_batch, train_y_batch = \
     tf.train.batch([xy[0:-1], xy[:-1]], batch_size=10)
 
 # placeholders for a tensor that will be always fed
-X = tf.placeholder(tf.float32, shape=[None, 3])
+X = tf.placeholder(tf.float32, shape=[None, 3]) # [# of instances, 3 features]
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight') # [# of X feature, # of Y]
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis
+hypothesis = tf.matmul(X, W) + b
+
+# Simplified cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+# Start populating the filename queue.
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+for step in range(2001):
+    # 파일에서 batch를 읽어옴.
+    x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
+    # 학습을 시킬 때 batch를 넘겨줌.
+    cost_val, hy_val, _ = sess.run(
+        [cost, hypothesis, train],
+        feed_dict={X: x_batch, Y: y_batch})
+    if step % 10 == 0 :
+        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+
+# suffle_batch 등 batch의 순서를 섞는 함수 등도 다양하게 존재함.
+
+
+
