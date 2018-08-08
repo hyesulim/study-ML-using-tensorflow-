5ff08ec953ed924bd1bd4f7ce8d87c09bf36a5e1
limhyesu98 <redbaloon17@gmail.com>
2018-08-08 17:31:21 +0900

    typo

--- Lab 07-2 linear_regression_without_min_max.py
+++ Lab 07-2 linear_regression_without_min_max.py
@@ -39,7 +39,7 @@ sess.run(tf.global_variables_initializer())
 for step in range(2001):
     cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
                                    feed_dict={X: x_data, Y: y_data})
-    #print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+    print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
 
 '''
 before normalization
