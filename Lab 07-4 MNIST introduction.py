bbaaa5f0be177b41f00a03e85d6d4ed14a75c5a9
limhyesu98 <redbaloon17@gmail.com>
2018-08-08 18:13:44 +0900

    continuing

--- Lab 07-4 MNIST introduction.py
+++ Lab 07-4 MNIST introduction.py
@@ -20,3 +20,27 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 # one_hot 형태의 hypothesis와 Y의 값이 같은지 확인.
 is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+# Training epoch/batch
+# one epoch : 전체 데이터 셋을 1번 다 학습시킨다
+# batch size : the number of training examples in one pass
+# 1000 training examples, 500 batch size -> 2 iterations to complete 1 epoch
+
+training_epoch = 15
+batch_size = 100
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(training_epoch):
+        avg_cost = 0
+        # total_batch : number of iterations
+        total_batch = int(mnist.train.num_examples / batch_size)
+
+        # 아래 loop가 끝나면 1 epoch이 끝난 것임.
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            c, _ = sess.run([cost, optimizer], feed_dict={X: batch_xs, Y: batch_ys})
+            avg_cost += c / total_batch
+
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
