81b019fd55a5b1194eb063a6ac85152ecb4f18a9
limhyesu98 <redbaloon17@gmail.com>
2018-08-20 12:01:16 +0900

    Lab 09-3 Tensorboard 사용 코드

--- Lab 09-3 using TensorBoard.py
+++ Lab 09-3 using TensorBoard.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-learning_rate = 0.01
+learning_rate = 0.1
 
 x_data = [[0, 0],
           [0, 1],
@@ -29,7 +29,7 @@ with tf.name_scope("layer1"):
 with tf.name_scope("layer2"):
     W2 = tf.Variable(tf.random_normal([2, 1]), name='weight2')
     b2 = tf.Variable(tf.random_normal([1]), name='bias2')
-    hypothesis = tf.sigmoid(tf.matmul(X, W2) + b2)
+    hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
     w2_hist = tf.summary.histogram("weights2", W2)
     b2_hist = tf.summary.histogram("biases2", b2)
@@ -46,9 +46,29 @@ with tf.name_scope("train"):
 # Accuracy computation
 # True if hypothesis > 0.5 else False
 predicted = tf.cast(hypothesis>0.5, dtype=tf.float32)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, hypothesis), dtype=tf.float32))
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 accuracy_summ = tf.summary.scalar("accuracy", accuracy)
 
 # Launch graph
 with tf.Session() as sess:
-    
+    # tensorboard --logdir=./logs/xor_logs
+    merged_summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs/xor_logs_r0_01")
+    writer.add_graph(sess.graph)
+
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        summary, _ = sess.run([merged_summary, train], feed_dict={X: x_data,
+                                                                  Y: y_data})
+        writer.add_summary(summary, global_step=step)
+
+        if step % 100 == 0:
+            print(step, sess.run(cost, feed_dict={
+                X: x_data, Y: y_data}), sess.run([W1, W2]))
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data,
+                                                                     Y: y_data})
+    print("\nHypothesis: ",h,"\nCorrect: ", c,"\nAccuracy: ", a)
